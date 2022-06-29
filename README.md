a2097f591cd94addbd6ca7d370ec5e379fa1cc92
Muli <winnie@alliedcommerce.com>
2022-06-29 16:33:19 +0300

    First stable commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# printmc-Print3
+
+Printing usind the StarPRNT SDK on mc-Print 3
+Create 3 buttons;PrintSample, Connect and Drawer
+
+## Getting Started
+
+This project is a starting point to using the StarPrint SDK on the mc-Print 3 receipt printer.
+Basic flutter app. Currently working on 3 buttons.Check version in Xcode and Flutter for compatibility.
+Xcode version 12.1
+Flutter 2.10.5
+
---
+++ build/windows/cmake_install.cmake
@@ -0,0 +1,112 @@
+# Install script for directory: F:/mc-Print3/StarPrintSDK_Flutter_iOS/windows
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "$<TARGET_FILE_DIR:mc-print 3>")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/flutter/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/runner/cmake_install.cmake")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "" TYPE EXECUTABLE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/runner/Debug/printmc-Print3.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Pp][Rr][Oo][Ff][Ii][Ll][Ee])$")
+    file(INSTALL DESTINATION "" TYPE EXECUTABLE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/runner/Profile/printmc-Print3.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "" TYPE EXECUTABLE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/runner/Release/printmc-Print3.exe")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/windows/flutter/ephemeral/icudtl.dat")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Pp][Rr][Oo][Ff][Ii][Ll][Ee])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/windows/flutter/ephemeral/icudtl.dat")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/windows/flutter/ephemeral/icudtl.dat")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/windows/flutter/ephemeral/flutter_windows.dll")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Pp][Rr][Oo][Ff][Ii][Ll][Ee])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/windows/flutter/ephemeral/flutter_windows.dll")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/windows/flutter/ephemeral/flutter_windows.dll")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Pp][Rr][Oo][Ff][Ii][Ll][Ee])$")
+    
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "" TYPE DIRECTORY FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build//flutter_assets")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Pp][Rr][Oo][Ff][Ii][Ll][Ee])$")
+    file(INSTALL DESTINATION "" TYPE DIRECTORY FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build//flutter_assets")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "" TYPE DIRECTORY FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build//flutter_assets")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xRuntimex" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Pp][Rr][Oo][Ff][Ii][Ll][Ee])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/app.so")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "" TYPE FILE FILES "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/app.so")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "F:/mc-Print3/StarPrintSDK_Flutter_iOS/build/windows/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
